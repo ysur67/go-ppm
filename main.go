e5eb021d7e034e72db226818e35f30e744e644d9
ysur67 <rus.muratow@gmail.com>
2021-11-23 15:48:50 +0300

    code cleanup

--- main.go
+++ main.go
@@ -5,18 +5,16 @@ import (
 	"os"
 )
 
-func writeCircle(buffer *[]uint, width, height, foreground, background, radius int) {
-	centerX := width
-	centerY := height
+func writeCircle(buffer *[]uint, width, height int, foreground, background uint, radius int) {
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			dx := centerX - x*2 - 1
-			dy := centerY - y*2 - 1
+			dx := width - x*2 - 1
+			dy := height - y*2 - 1
 			r := radius * radius
 			if (dx*dx)+(dy*dy) <= (r) {
-				(*buffer)[y*width+x] = uint(foreground)
+				(*buffer)[y*width+x] = foreground
 			} else {
-				(*buffer)[y*width+x] = uint(background)
+				(*buffer)[y*width+x] = background
 			}
 		}
 	}
@@ -33,9 +31,9 @@ func dumpToPpm(buffer *[]uint, filename string, width, height int) error {
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			pixel := (*buffer)[y*width+x]
-			r := pixel >> 8 * 0 & 0xff
-			g := pixel >> 8 * 1 & 0xff
-			b := pixel >> 8 * 2 & 0xff
+			r := pixel >> 8 * 0 & 0xFF
+			g := pixel >> 8 * 1 & 0xFF
+			b := pixel >> 8 * 2 & 0xFF
 			file.WriteString(fmt.Sprintf("%d %d %d \n", r, g, b))
 		}
 	}
@@ -45,8 +43,8 @@ func dumpToPpm(buffer *[]uint, filename string, width, height int) error {
 func main() {
 	WIDTH := 800
 	HEIGHT := 600
-	FOREGROUND := 0xFFFFFF
-	BACKGROUND := 0x000000
+	var FOREGROUND uint = 0xFFFFFF
+	var BACKGROUND uint = 0x000000
 	var buffer = make([]uint, WIDTH*HEIGHT)
 	writeCircle(&buffer, WIDTH, HEIGHT, FOREGROUND, BACKGROUND, HEIGHT/2)
 	if err := dumpToPpm(&buffer, "circle.ppm", WIDTH, HEIGHT); err != nil {
